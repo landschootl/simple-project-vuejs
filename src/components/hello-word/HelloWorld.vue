76ced587f64b92ae78d0fbf37e49dbfa32fca599
landschootl <ludovic.landschoot@gmail.com>
2019-07-05 02:10:51 +0200

    chore (project): init

---
+++ src/components/hello-word/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template src="./HelloWord.html"> </template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./HelloWord.scss" lang="scss"></style>
